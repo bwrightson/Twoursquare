2de2d6c4c02164276a034e2ee35b175c58728f3d
Brian Wrightson <brian.wrightson@gmail.com>
2012-03-20 21:05:39 -0400

    Add Ruby on Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twoursquare::Application.config.secret_token = '22de819f655ce6fecde67237b967e08defccf5baf8c7d41e973ccf7fc8c5915e4e9fd765db7d104dd0b7ac334be3bfb56eed3cac8f91be6a620abad74a111dbb'
