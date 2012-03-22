ff902b2602168f5ae12cf393ee4b6622ca29ce28
Brian Wrightson <brian.wrightson@gmail.com>
2012-03-22 18:05:58 -0400

    Add check-in functionality

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -3,15 +3,35 @@ class FoursquareController < ApplicationController
     message = params["TranscriptionText"]
     phone = params["Caller"]
     phone = phone.gsub('+1', '')
-    user = User.where('phone' => phone)
+    user = User.find_by_phone(phone)
     chunks = message.split(' in ')
-    venue = chunks[0]
+    raw_query = chunks[0].downcase
+    query = URI.escape(raw_query)
     city = URI.escape(chunks[1])
     url = "http://where.yahooapis.com/geocode?q=" + city + "&appid=pi5fY574"
-    location = Curl::Easy.perform(url)
-    location = location.body_str
+    location = Curl::Easy.perform(url).body_str
     @xml_doc = Nokogiri::XML(location)
-    lat = @xml_doc.at_css("Result latitude").inner_text()
-    lon = @xml_doc.at_css("Result longitude").inner_text()
+    lat = URI.escape(@xml_doc.at_css("Result latitude").inner_text())
+    lon = URI.escape(@xml_doc.at_css("Result longitude").inner_text())
+    url = "https://api.foursquare.com/v2/venues/search?ll="
+    url += lat + "," + lon + "&query=" + query
+    url += "&oauth_token=" + user.token + "&v=20120322"
+    venue_list = JSON.parse(Curl::Easy.perform(url).body_str)
+    venue_id = ""
+    venue_list["response"]["venues"].each do |venue|
+      if venue["name"].downcase == raw_query
+        venue_id = venue["id"]
+      end
+    end
+    if venue_id != ""
+       url = "https://api.foursquare.com/v2/checkins/add"
+       post_params = [
+         Curl::PostField.content('venueId', venue_id),
+	 Curl::PostField.content('oauth_token', user.token),
+       	 Curl::PostField.content('v', 20120322)
+       ]
+       checkin_response = JSON.parse(Curl::Easy.http_post(url, *post_params).body_str)
+    end
+    binding.pry
   end
 end
